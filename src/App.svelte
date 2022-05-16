d44695c93bc28693d956eefab460046e68dbc912
Neyl <neyl.mage@seznam.cz>
2022-05-16 21:52:42 +0200

    Add search player homepage.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script>
 	import AOEIV from "./AOEIV.svelte";
 	import AOE4WORLD from "./AOE4WORLD.svelte";
+	import Search from "./Search.svelte";
 
 	const component_to_render = "AOE4WORLD";
+
+	const url = new URL(location);
+	const is_search = !url.search
+	
 </script>
 
-{#if component_to_render === "AOEIV"}
-	<AOEIV />
+{#if is_search}
+	<Search />
 {:else}
-	<AOE4WORLD />
+	{#if component_to_render === "AOEIV"}
+		<AOEIV />
+	{:else}
+		<AOE4WORLD />
+	{/if}
 {/if}
